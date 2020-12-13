5423d2017fcb90a6eb5c78a7049f067eb14ca3f0
Ibrahem Wattaweda <wattad111@gmail.com>
2020-12-13 16:33:11 +0200

    asdfsdsdf

---
+++ MyCinema/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyCinema.Models;
+using System.Data.SqlClient;
+namespace MyCinema.Controllers
+{
+    public class UserController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id=0)
+        {
+            User user = new User();
+            return View(user);
+        }
+        [HttpPost]
+
+        public ActionResult AddOrEdit(User userModel)
+        {
+
+            using (DbModel dbModel = new DbModel())
+            {
+                if (dbModel.Users.Any(x => x.username == userModel.username))
+                {
+                    ViewBag.DuplicateMessage = "Username already exist.";
+                    return View("AddOrEdit", userModel);
+                }
+                dbModel.Users.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registeration Successful.";
+            return View("AddOrEdit", new User());
+        }
+
+        
+
+    }
+}
---
+++ MyCinema/Controllers/accountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using MyCinema.Models;
+
+namespace MyCinema.Controllers
+{
+    public class accountController : Controller
+    {
+        SqlConnection con = new SqlConnection("Data Source=IBRAHEMA98W\\MSSQLSERVER01;Initial Catalog=tempdb;Integrated Security=True;");
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            string check = " select count(*) from [User] where username ='" + acc.username + "'and password= '" + acc.password + "' ";
+            SqlCommand com = new SqlCommand(check, con);
+            con.Open();
+            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+            con.Close();
+
+            if (temp == 1)
+            {
+                return View("Successful");
+            }
+            else
+            {
+                ViewBag.DuplicateMessage = "Wrong Username or password.";
+                return View("Login");
+            }
+        }
+    }
+}
